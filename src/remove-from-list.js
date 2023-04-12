2dabce15dd985815432debb94b1df04c2129e707
Yuliya Harbacheuskaya <yulia.harbacheuskaya@gmail.com>
2023-04-12 09:33:30 -0400

    feat: solve singly linked list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require("../extensions/list-node.js");
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,11 +22,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(list, element) {
+	if (!list) return null;
+	if (list.value === element) {
+		return removeKFromList(list.next, element);
+	}
+	list.next = removeKFromList(list.next, element);
+	return list;
 }
 
 module.exports = {
-  removeKFromList
+	removeKFromList,
 };
